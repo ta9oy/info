6e637002fa415970cb1d0d6c533eb58e3ba6b1f0
Asraful Islam Tanoy <89956899+ta9oy@users.noreply.github.com>
2026-01-13 10:22:53 +0000

    Remove "Go" buttons from article sections in index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -399,18 +399,3 @@
 					});
 
 })(jQuery);
-
-// Smooth scroll for internal links that should not change the hash.
-(function($){
-	$(function(){
-		$(document).on('click', 'a.internal-no-hash', function(e){
-			var href = $(this).attr('href');
-			if (!href || href.charAt(0) !== '#') return;
-			var $target = $(href);
-			if ($target.length) {
-				e.preventDefault();
-				$('html,body').animate({ scrollTop: $target.offset().top }, 600);
-			}
-		});
-	});
-})(jQuery);
